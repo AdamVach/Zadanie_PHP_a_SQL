fa9fe76456eaa1210d16403ad02e73eef3a2d27d
AdamVach <avachm@gmail.com>
2026-04-16 10:20:17 +0200

    databaza opravy

--- Databaza.php
+++ Databaza.php
@@ -11,34 +11,48 @@
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
-    $sql = "CREATE DATABASE IF NOT EXISTS pouzivatelia;
-    USE pouzivatelia;
+   
+    $sql = "CREATE DATABASE IF NOT EXISTS databaza_knih;
+    USE databaza_knih;
     CREATE TABLE IF NOT EXISTS pouzivatel (
         pouzivatel_ID INT(6) AUTO_INCREMENT PRIMARY KEY,
         meno VARCHAR(30) NOT NULL,
         email VARCHAR(50) NOT NULL,
         heslo VARCHAR(255) NOT NULL
     );
-    INSERT INTO pouzivatel (id, meno, priezvisko, email) VALUES
-    (NULL, 'Ján', 'jan.novak@example.com', password_hash('heslo123', PASSWORD_DEFAULT)),
-    (NULL, 'Petra', 'petra.kovacova@example.com', password_hash('tajneheslo', PASSWORD_DEFAULT)),
-    (NULL, 'Martin', 'martin.horvath@example.com', password_hash('mojeheslo', PASSWORD_DEFAULT)),
-    (NULL, 'Lucia', 'lucia.vargova@example.com', password_hash('superheslo', PASSWORD_DEFAULT)),;
+    
 
     CREATE TABLE IF NOT EXISTS knihy (
         kniha_ID INT(6) AUTO_INCREMENT PRIMARY KEY,
         nazov VARCHAR(50) NOT NULL,
         autor VARCHAR(50) NOT NULL,
         rok_vydania YEAR NOT NULL
-    );
-    INSERT INTO knihy (kniha_id, nazov, autor, rok_vydania) VALUES
-    (NULL, 'Na západe nič nové', 'Erich Maria Remarque', 1929),
-    (NULL, '1984', 'George Orwell', 1949),
-    (NULL, 'Malý princ', 'Antoine de Saint-Exupéry', 1943),
-    (NULL, 'Pýcha a predsudok', 'Jane Austen', 1813),
-    (NULL, 'Zločin a trest', 'Fjodor Michajlovič Dostojevskij', 1866);";
-    mysqli_multi_query($conn, $sql);
-    mysqli_close($conn);
+    );";
+    
+    if (mysqli_multi_query($conn, $sql)) {
+        $sql = "USE databaza_knih; SELECT COUNT(pouzivatel_ID) AS pouzivatel_count FROM databazy_knih;";
+        $result = mysqli_query($conn, $sql);
+        if ($result) {
+            $row = mysqli_fetch_assoc($result);
+            if ($row['pouzivatel_count'] == 0) {
+                $defaultUserSql = "USE databaza_knih;
+                INSERT INTO pouzivatel (pouzivatel_ID, meno, email, heslo) VALUES 
+                (NULL, 'Ján', 'jan.novak@example.com','" . password_hash('admin', PASSWORD_DEFAULT) . "'),
+                (NULL, 'Petra', 'petra.kovacova@example.com','" . password_hash('petra', PASSWORD_DEFAULT) . "');
+
+                INSERT INTO knihy (kniha_ID, nazov, autor, rok_vydania) VALUES
+                (NULL, 'Na západe nič nové', 'Erich Maria Remarque', 1929),
+                (NULL, '1984', 'George Orwell', 1949),
+                (NULL, 'Malý princ', 'Antoine de Saint-Exupéry', 1943),
+                (NULL, 'Pýcha a predsudok', 'Jane Austen', 1813),
+                (NULL, 'Zločin a trest', 'Fjodor Michajlovič Dostojevskij', 1866);";
+                if (!mysqli_multi_query($conn, $defaultUserSql)) {
+                    die("Error inserting default users: " . mysqli_error($conn));
+                }
+            }
+        }
+    }
+    
     ?>
 </body>
 </html>
